6475c78c86fb56497d4694ea417c18b52784180d
Iran <iranildojunior777@gmail.com>
2025-05-03 09:46:42 -0300

    fix(main.go) chage window name

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "mov-converter",
+		Title:  "Zappy Mov Converter",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
